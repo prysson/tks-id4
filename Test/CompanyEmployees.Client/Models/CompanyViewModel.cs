0810696c7bb83acc81253b4614df8b829ec417b2
prysson <31078906+prysson@users.noreply.github.com>
2021-02-10 17:59:36 -0600

    Updated Test ID4

---
+++ Test/CompanyEmployees.Client/Models/CompanyViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace CompanyEmployees.Client.Models
+{
+    public class CompanyViewModel
+    {
+        public string Name { get; set; }
+        public string FullAddress { get; set; }
+    }
+}
---
+++ Test/CompanyEmployees.IDP/Config.cs
@@ -0,0 +1,44 @@
+﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+
+using IdentityServer4;
+using IdentityServer4.Models;
+using System.Collections.Generic;
+
+namespace CompanyEmployees.IDP
+{
+    public static class Config
+    {
+        public static IEnumerable<IdentityResource> IdentityResources =>
+            new IdentityResource[]
+            { 
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile()
+            };
+
+        public static IEnumerable<ApiScope> ApiScopes =>
+            new ApiScope[]
+            { };
+
+        public static IEnumerable<ApiResource> Apis =>
+           new ApiResource[]
+           { };
+
+        public static IEnumerable<Client> Clients =>
+            new Client[] 
+            { 
+                new Client
+                {
+                    ClientName = "CompanyEmployeeClient",
+                    ClientId = "companyemployeeclient",
+                    AllowedGrantTypes = GrantTypes.Code,
+                    RedirectUris = new List<string>{ "httpd://5010/signin=oidc" },
+                    AllowedScopes = { IdentityServerConstants.StandardScopes.OpenId, IdentityServerConstants.StandardScopes.Profile },
+                    ClientSecrets = { new Secret("CompanyEMployeeCLientSecret".Sha512())},
+                    RequirePkce = false,
+                    RequireConsent = true
+                }
+            };
+    }
+}
